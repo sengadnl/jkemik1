8d9fbc193b90dee6798bc1bfd69bd907a92247d1
sengadnl@gmail.com <sengadnl@gmail.com@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2012-11-27 21:12:12 +0000

    adding a file

---
+++ jkemik/trunk/controler/ModeToggleListener.java
@@ -0,0 +1,47 @@
+package controler;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JCheckBox;
+
+import view.BoardFrame;
+
+public class ModeToggleListener implements MouseListener{
+	public ModeToggleListener(JCheckBox manual){
+		
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		if(BoardFrame.manual.isSelected()){
+			JKemik.settings_t.setAutoCapture(false);
+			JKemik.settings_t.setAutoPass(false);
+			BoardFrame.showControlButtons();
+		}else{
+			JKemik.settings_t.setAutoCapture(true);
+			JKemik.settings_t.setAutoPass(true);
+			BoardFrame.showControlButtons();
+		}
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
