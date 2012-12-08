f2ac0a591ef74a21194e8f429e71e61bb2fd9f11
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2012-12-08 05:23:13 +0000

    Tag release 3.0.179

---
+++ tags/REL-3.0.179/api/STemplate.java
@@ -0,0 +1,115 @@
+package api;
+
+import java.io.Serializable;
+
+public class STemplate implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean autoCapture = true;
+	private boolean manualCapture = false;
+	private boolean autoPass = true;
+	private String theme = "Geeky";
+	private int maxWinVal = 4;
+
+	private boolean[] memo = { true, false };
+
+	public STemplate() {
+
+	}
+
+	public boolean[] getMemo() {
+		return memo;
+	}
+
+	public void setMemo(boolean c, boolean p) {
+		if (c) {
+			this.memo[0] = true;
+		} else {
+			this.memo[0] = false;
+		}
+		if (p) {
+			this.memo[1] = true;
+		} else {
+			this.memo[1] = false;
+		}
+	}
+
+	public void restaureMemo() {
+		if (this.memo[0]) {
+			this.autoCapture = true;
+		} else {
+			this.autoCapture = false;
+		}
+
+		if (this.memo[1]) {
+			this.autoPass = true;
+		} else {
+			this.autoPass = false;
+		}
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	public int getMaxWinVal() {
+		return maxWinVal;
+	}
+
+	public void setMaxWinVal(int maxWinVal) {
+		this.maxWinVal = maxWinVal;
+	}
+
+	public boolean isManualCapture() {
+		return manualCapture;
+	}
+
+	public void setManualCapture(boolean manualCapture) {
+		this.manualCapture = manualCapture;
+	}
+
+	public String getAutoCaptureStatus() {
+		if (autoCapture) {
+			return "AUTO";
+		}
+		return "MANUAL";
+	}
+
+	public String getAutoPassStatus() {
+		if (autoPass) {
+			return "AUTO";
+		}
+		return "MANUAL";
+	}
+
+	public String getManualCaptureStatus() {
+		if (manualCapture) {
+			return "AUTO";
+		}
+		return "MANUAL";
+	}
+
+	public boolean isAutoCapture() {
+		return autoCapture;
+	}
+
+	public void setAutoCapture(boolean autoCapture) {
+		this.autoCapture = autoCapture;
+	}
+
+	public boolean isAutoPass() {
+		return autoPass;
+	}
+
+	public void setAutoPass(boolean autoPass) {
+		this.autoPass = autoPass;
+	}
+
+}
---
+++ tags/REL-3.0.179/utilities/ValidateInput.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package utilities;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author dalet
+ * 
+ */
+public class ValidateInput {
+	private static boolean PlayerName(String name1, String name2) {
+		try {
+			if (name1.equals("") || name2.equals("")) {
+				JOptionPane.showMessageDialog(null, "Empty String error",
+						"Wrong Input", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+			if (!(name1.length() <= 10) || !(name2.length() <= 10)) {
+				JOptionPane.showMessageDialog(null,
+						"Name must be < 5 characters",
+						"Wrong Input", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "" + e.getMessage());
+		}
+		return true;
+	}
+
+	public static boolean names(String name1, String name2) {
+		name1 = name1.toUpperCase();
+		name2 = name2.toUpperCase();
+		if (!PlayerName(name1, name2)) {
+			return false;
+		}
+		if (name1.equals(name2)) {
+			JOptionPane.showMessageDialog(null, "Ellegal name combination!",
+					"Wrong Input", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateColors(Color p1, Color p2) {
+		if (p1.equals(p2)) {
+			JOptionPane.showMessageDialog(null, "Ellegal color combination!",
+					"Wrong Input", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateScreenResolution(int width, int height) {
+		if ((width < 1280) && (height < 800)) {
+			JOptionPane.showMessageDialog(null, "Your screen resolution must be "
+					+ width + "X" + height + " or greater.",
+					"Wrong Input", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		return true;
+	}
+}
