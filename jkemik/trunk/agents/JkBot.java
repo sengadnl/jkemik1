c465afccf736ad1dc8571880e329effcc176f853
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-02-14 04:32:21 +0000

    Added the agent

---
+++ jkemik/trunk/agents/JkBot.java
@@ -0,0 +1,22 @@
+package agents;
+import java.awt.*;
+
+//import api.AbstractPlayer;
+import api.Player;
+
+
+public class JkBot extends Player implements AgentAction{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public JkBot(Color color, String name) {
+		super(color, name);
+	}
+	@Override
+	public boolean play() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
