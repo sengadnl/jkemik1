cefae43c987e41a9c82c24d77abdb9ce09f694fb
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-02-23 17:55:47 +0000

    Defined game type listeners

--- jkemik/trunk/Events/HvsHListener.java
+++ jkemik/trunk/Events/HvsHListener.java
@@ -1,5 +1,52 @@
 package Events;
 
-public class HvsHListener {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JRadioButton;
+
+public class HvsHListener implements MouseListener{
+	private JRadioButton gtype;
+	public HvsHListener(JRadioButton gtype){
+		setGtype(gtype);
+	}
+	
+	public JRadioButton getGtype() {
+		return gtype;
+	}
+
+	public void setGtype(JRadioButton gtype) {
+		this.gtype = gtype;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- jkemik/trunk/Events/NetworkGameListener.java
+++ jkemik/trunk/Events/NetworkGameListener.java
@@ -1,5 +1,52 @@
 package Events;
 
-public class NetworkGameListener {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JRadioButton;
+
+public class NetworkGameListener implements MouseListener{
+	private JRadioButton network;
+	public NetworkGameListener(JRadioButton network){
+		setNetwork(network);
+	}
+	
+	public JRadioButton getNetwork() {
+		return network;
+	}
+
+	public void setNetwork(JRadioButton network) {
+		this.network = network;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
