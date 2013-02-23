4e9a52906417b3ebae922bedf9cffbc6eadc5292
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-02-23 17:55:47 +0000

    Defined game type listeners

--- jkemik/trunk/Events/HvsAIListener.java
+++ jkemik/trunk/Events/HvsAIListener.java
@@ -1,5 +1,53 @@
 package Events;
 
-public class HvsAIListener {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JRadioButton;
+
+public class HvsAIListener implements MouseListener {
+	private JRadioButton gtype;
+	
+	public HvsAIListener(JRadioButton gtype) {
+		setNetwork(gtype);
+	}
+	
+	public JRadioButton getNetwork() {
+		return gtype;
+	}
+
+	public void setNetwork(JRadioButton network) {
+		this.gtype = network;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
