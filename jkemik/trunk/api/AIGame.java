20e74ef85f2ca9e1e7a83d24ede06d4bbe3243c1
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-02-28 05:42:27 +0000

    Game type

--- jkemik/trunk/api/AIGame.java
+++ jkemik/trunk/api/AIGame.java
@@ -1,15 +1,15 @@
 package api;
 
+import agents.JkBot;
+
 public class AIGame extends AbstractGame{
-
-	public AIGame(Player player1, Player player2) {
-		super(player1, player2);
-		// TODO Auto-generated constructor stub
-	}
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	public AIGame(Player player1, JkBot player2) {
+		super(player1, player2);
+		
+		// TODO Auto-generated constructor stub
+	}
 }
