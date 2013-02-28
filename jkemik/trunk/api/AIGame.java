571c1897367c2482f81e055aaeb6be9be06c4d67
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-02-28 05:14:46 +0000

    new classes

---
+++ jkemik/trunk/api/AIGame.java
@@ -0,0 +1,15 @@
+package api;
+
+public class AIGame extends AbstractGame{
+
+	public AIGame(Player player1, Player player2) {
+		super(player1, player2);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
