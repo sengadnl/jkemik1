4c6e4cc55891abff1c7ed483d378801fa939ce9f
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-03-28 04:07:06 +0000

    Creating release branch 4.0.207

---
+++ branches/RB-4.0.206/trunk/Events/HvsHListener.java
@@ -0,0 +1,52 @@
+package Events;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JRadioButton;
+
+import controler.JKemik;
+
+public class HvsHListener implements MouseListener{
+	private JRadioButton gtype;
+	public HvsHListener(JRadioButton gtype){
+		setGtype(gtype);
+	}
+	
+	public JRadioButton getGtype() {
+		return gtype;
+	}
+
+	public void setGtype(JRadioButton gtype) {
+		this.gtype = gtype;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		JKemik.settings_t.setHh(true);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ branches/RB-4.0.206/trunk/utilities/Globals.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package utilities;
+import java.awt.Color;
+
+/**
+ * @author Daniel Senga
+ *
+ */
+public class Globals {
+	public static final double PLR_N_LEN = 5;
+	public static final double FADE_VARIANT = .35;//for fading colors
+	public static final double LABEL_VARIANT = 120;//for fading colors
+	public static final int FADE_THRESHOLD = 50;
+	public static final Color BTN_FRD_COLOR = new Color(220,220,10);
+	public static final Color BTN_BGD_COLOR = new Color(60,0,0);
+	public static final Color PENALTY_COL = new Color(224,27,208);
+	public static final String templateObjectFile = "/tmp/template.obj";
+	public static final String tempFile = "tmp";
+	public static final String gameObjectFile = "/tmp/game.obj";
+	public static final String settingsTemplateObjectFile = "/tmp/stemplate.obj";
+	public static final String VERSION = "Beta-4.0.260";
+	public static final int RED_INDEX = 0;
+	public static final int WHITE_INDEX = 4;
+	public static final int GREEN_INDEX = 5;
+	public static final int YELLOW_INDEX = 3;
+	public static final double MAX_WIN = .1;
+	public static final int LIGHT_PURPPLE_INDEX = 3;
+	public static final int DARK_PURPPLE_INDEX = 9;
+	public static final double FRAME_WIDTH = 1280;
+	public static final double FRAME_HEIGHT = 800;
+	
+	public static final Color[] CHEMIK_COLOR = {
+		new Color(250, 0, 255),
+		new Color(170, 200, 190),
+		new Color(255, 255, 100)};
+	public static final Color[] ORIGINE_COLOR = {
+		new Color(232, 216, 37),
+		new Color(99, 196, 235),
+		new Color(45, 214, 68)};
+	public static final Color[] CLASSIC_COLOR = {
+		new Color(0, 255, 0),
+		new Color(250, 50, 50),
+		new Color(100, 250, 110)};
+	public static final Color[] GEECKY_COLOR = {
+		new Color(97, 237, 135),
+		new Color(247, 174, 101),
+		new Color(170, 85, 173)};
+	public static final String[] code = {"en","fr","de"};
+	public static final String[] laguageNames = {"ENGLISH","FRANCAIS","DEUTSCH"};
+	
+}
