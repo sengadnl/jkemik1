b02a4a97262af3a43903cc3c2d64d2fd12ffc439
sengadnl <sengadnl@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2013-03-28 04:12:33 +0000

    Tag release 4.0.207

---
+++ Events/ManualCaptureButtonListener.java
@@ -0,0 +1,52 @@
+package Events;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JButton;
+
+import controler.JKemik;
+
+import view.BoardFrame;
+import view.Grid;
+
+public class ManualCaptureButtonListener implements MouseListener{
+	private JButton capture;
+
+	public ManualCaptureButtonListener(JButton capture) {
+		this.setDebug(capture);
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		Grid.setManualc(true);
+		Grid.selectPoint = true;
+		JKemik.game.getCurrentP().setOrigin(JKemik.game.getLastp());
+		JKemik.game.getCurrentP().getSelected().add(JKemik.game.getLastp());
+		Grid.setSelectedP(JKemik.game.getLastp());
+		JKemik.game.setEmbuche_on(false);
+		BoardFrame.grid.repaint();
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+
+	}
+
+	public JButton getDebug() {
+		return capture;
+	}
+
+	public void setDebug(JButton debug) {
+		this.capture = debug;
+	}
+}
---
+++ agents/AgentAction.java
@@ -0,0 +1,5 @@
+package agents;
+
+public interface AgentAction {
+	public boolean play();
+}
