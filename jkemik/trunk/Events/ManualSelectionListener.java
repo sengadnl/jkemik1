85ca628fd0c2d570bd900261187d675058c5f1e3
sengadnl@gmail.com <sengadnl@gmail.com@448f8b3a-472d-e0b5-a73d-fc981c7fd2c4>
2014-06-27 05:42:42 +0000

    fixed gui

--- jkemik/trunk/Events/ManualSelectionListener.java
+++ jkemik/trunk/Events/ManualSelectionListener.java
@@ -20,16 +20,15 @@ public class ManualSelectionListener implements MouseListener {
 		this.setDebug(capture);
 	}
 
-        @Override
 	public void mouseClicked(MouseEvent arg0) {
 		if (capture.isSelected()) {
-                    Grid.setManualc(true);
-                    Grid.selectPoint = true;
-                    BoardFrame.mode.setVisible(false);
-                    BoardFrame.pass_turn.setVisible(false);
-                    JKemik.game.setEmbuche_on(false);
+			Grid.setManualc(true);
+			Grid.selectPoint = true;
+			BoardFrame.mode.setVisible(false);
+			BoardFrame.pass_turn.setVisible(false);
+			JKemik.game.setEmbuche_on(false);
 		} else {
-			JKemik.game.getCurrentP().setSelected(new ArrayList<>());
+			JKemik.game.getCurrentP().setSelected(new ArrayList<Point>());
 			JKemik.view.repaint();
 			BoardFrame.grid.drawn = false;
 			Grid.manualc = false;
@@ -39,23 +38,19 @@ public class ManualSelectionListener implements MouseListener {
 		}
 	}
 
-        @Override
 	public void mouseEntered(MouseEvent arg0) {
 		//capture.setToolTipText("Check to select manually.");
 		capture.setToolTipText(BoardFrame.messages.getString("checkSelect"));
 	}
 
-        @Override
 	public void mouseExited(MouseEvent arg0) {
 
 	}
 
-        @Override
 	public void mousePressed(MouseEvent arg0) {
 
 	}
 
-        @Override
 	public void mouseReleased(MouseEvent arg0) {
 
 	}
@@ -64,7 +59,7 @@ public class ManualSelectionListener implements MouseListener {
 		return capture;
 	}
 
-	private void setDebug(JCheckBox debug) {
+	public void setDebug(JCheckBox debug) {
 		this.capture = debug;
 	}
 }
