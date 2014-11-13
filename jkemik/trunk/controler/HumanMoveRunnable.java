e160652f95f0a3e1fcb80f82301bac131bc91efd
sengadnl <sengadnl@gmail.com>
2014-11-13 12:58:51 +0000

    Restricted play turns such that player would be able to play consecutively

--- jkemik/trunk/controler/HumanMoveRunnable.java
+++ jkemik/trunk/controler/HumanMoveRunnable.java
@@ -20,7 +20,6 @@ import javax.swing.JOptionPane;
 import utilities.Globals;
 import view.BoardFrame;
 import view.Grid;
-
 /**
  *
  * @author admin
@@ -34,24 +33,21 @@ public class HumanMoveRunnable implements Runnable{
         humanMoveLock.lock();
          try {
              
-            AIGame game = (AIGame) JKemik.getGame();
-            Player current;
-            current = (Player) game.getHuman();
-            JkBot bot = (JkBot) game.getMachine();
+            AIGame game;Player current;
             
-            //if(current.isTurn() && current.getPlay_flag() == 0){
+            /*Initializing variables*/
+            game = (AIGame) JKemik.getGame();
+            current = (Player) game.getHuman();
+            
+            if(current.isTurn() && current.getPlay_flag() == 0){
                 // Allow mouse click
-            Grid.mouseclicked = true;
+                Grid.mouseclicked = true;
 
-            // Guess player's point
-            Grid.closestTo(Grid.x, Grid.y, (int) Grid.squareSize);
-            Point temp = new Point(Grid.x, Grid.y);
-
-            if (!game.getCollection().containsKey(temp.toString())) {
-                    System.out.println("HUMAN PLAYED...");
+                // Guess player's point
+                Grid.closestTo(Grid.x, Grid.y, (int) Grid.squareSize);
+                Point temp = new Point(Grid.x, Grid.y);
+                if (!game.getCollection().containsKey(temp.toString())) {
                     Grid.plotPoint = true;
-
-                    //BoardFrame.grid.repaint();
 
                     // Mark point as played
                     temp.setStatus(Globals.POINT_PLAYED);
@@ -75,63 +71,62 @@ public class HumanMoveRunnable implements Runnable{
                     game.getCurrentP().setPoints(1);
                     Grid.mouseMove = false;
 
-
                     BoardFrame.feedback(game.getCurrentP().getName() + " "
                                     + BoardFrame.messages.getString("feedback4"));
-            }
+            //}
 
-            BoardFrame.progressB.setVisible(true);
-            BoardFrame.progressB.setIndeterminate(true);
+                    BoardFrame.progressB.setVisible(true);
+                    BoardFrame.progressB.setIndeterminate(true);
 
-            if (game.isEmbuche_on()) {
-                if (JKemik.settings_t.isAutoCapture()) {
-                    Grid.cell = JKemik.embush(Grid.squareSize);
-                    if(Grid.cell != null){
+                    if (game.isEmbuche_on()) {
+                        if (JKemik.settings_t.isAutoCapture()) {
+                            Grid.cell = JKemik.embush(Grid.squareSize);
+                            if(Grid.cell != null){
+                                BoardFrame.grid.repaint();
+                            } 
+                        }
+                    }
+                    if (game.getCurrentP().isTurn()) {
+                            BoardFrame.grid.setMouseclicked(true);
+                    }
+
+                    BoardFrame.p1panel.updatePlayerPanel(game.getPlayer1());
+                    BoardFrame.p2panel.updatePlayerPanel(game.getPlayer2());
+                    BoardFrame.updateBoardStatus();
+
+                    Grid.setRefresh(true);
+                    BoardFrame.displayGrid(true);
+                    BoardFrame.grid.repaint((int)temp.getXC() - (int)Grid.squareSize * 2, (int)temp.getYC() - (int)Grid.squareSize * 2, (int)Grid.squareSize * 4, (int)Grid.squareSize * 4);
+                    
+                    game.switchPlayTurns();
+                    
+                    if (JKemik.checkEndGame()) {
+                        JOptionPane.showMessageDialog(null, "" + JKemik.getEndingMessage(),
+                                        " Win", JOptionPane.OK_OPTION);
+                        BoardFrame.feedback(JKemik.getEndingMessage());
+                        JKemik.getGame().setStatus(1);
+                        JKemik.createGame(JKemik.template, JKemik.settings_t);
+                        JKemik.settings_t.setGameSetupMode(true);
+                        // Reset game exit label
+                        BoardFrame.Game_status.setText("NEW");
+                        BoardFrame.uiLooksUpdate(JKemik.settings_t, JKemik.template);
+                        ViewEvents.uiEventUpdates(JKemik.settings_t, JKemik.template);
+
+                        Grid.setRefresh(true);
+                        BoardFrame.displayGrid(true);
                         BoardFrame.grid.repaint();
-                    } 
+                    }
+                    BoardFrame.progressB.setIndeterminate(false);
+                    BoardFrame.progressB.setVisible(false); 
                 }
             }
-            if (game.getCurrentP().isTurn()) {
-                    BoardFrame.grid.setMouseclicked(true);
-            }
-
-            BoardFrame.p1panel.updatePlayerPanel(game.getPlayer1());
-            BoardFrame.p2panel.updatePlayerPanel(game.getPlayer2());
-            BoardFrame.updateBoardStatus();
-
-            Grid.setRefresh(true);
-            BoardFrame.displayGrid(true);
-            BoardFrame.grid.repaint((int)temp.getXC() - (int)Grid.squareSize * 2, (int)temp.getYC() - (int)Grid.squareSize * 2, (int)Grid.squareSize * 4, (int)Grid.squareSize * 4);
-
-            if (JKemik.checkEndGame()) {
-                JOptionPane.showMessageDialog(null, "" + JKemik.getEndingMessage(),
-                                " Win", JOptionPane.OK_OPTION);
-                BoardFrame.feedback(JKemik.getEndingMessage());
-                JKemik.getGame().setStatus(1);
-                JKemik.createGame(JKemik.template, JKemik.settings_t);
-                JKemik.settings_t.setGameSetupMode(true);
-                // Reset game exit label
-                BoardFrame.Game_status.setText("NEW");
-                BoardFrame.uiLooksUpdate(JKemik.settings_t, JKemik.template);
-                ViewEvents.uiEventUpdates(JKemik.settings_t, JKemik.template);
-
-                Grid.setRefresh(true);
-                BoardFrame.displayGrid(true);
-                BoardFrame.grid.repaint();
-            }
-            BoardFrame.progressB.setIndeterminate(false);
-            BoardFrame.progressB.setVisible(false);
-
-            game.switchPlayTurns();
-           
             Thread.sleep(DELAY);
         } catch (InterruptedException ex) {
             Logger.getLogger(HumanMoveRunnable.class.getName()).log(Level.SEVERE, null, ex);
         }finally{
             humanMoveLock.unlock();
         }
-        //Thread.interrupted();
     }
-    private static final int DELAY = 300;
+    private static final int DELAY = 1000;
     private Lock humanMoveLock ;
 }
